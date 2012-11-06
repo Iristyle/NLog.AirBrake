3a20f47f29ab763330e9ed8c5ae02788ae70c34e
Iristyle <Iristyle@github>
2012-11-06 12:20:54 -0500

    Build process auto-publishes to nuget.org
     - Zipping of assets is no longer necessary
     - Copy nuspec file to build artifacts, use version from compiled dll
     - Added call to Publish-NugetPackage that build server will call

--- build/default.ps1
+++ build/default.ps1
@@ -20,14 +20,6 @@ function Resolve-Error
 Properties {
   $currentDirectory = Get-CurrentDirectory
   $BuildOutDir = Join-Path $currentDirectory 'BuildArtifacts'
-  $PackagesDirectory = Join-Path $currentDirectory 'Packages'
-  $7ZipPath = Join-Path $PackagesDirectory '7Zip\7za.exe'
-  #set by Jenkins when in his environment
-
-  $buildTag = @($Env:BUILD_TAG, 'NLogAirBrakeManualBuild') |
-    ? { -not [string]::IsNullOrEmpty($_) } | Select -First 1
-  $ZipPath = "$BuildOutDir\$buildTag.zip"
-  $ZipSymbolsPath = "$BuildOutDir\$buildTag-Symbols.zip"
 }
 
 Task default -Depends Test, Package
@@ -38,71 +30,24 @@ Task Compile -Depends Init,Clean {
   exec { msbuild build.proj }
 }
 
-filter binaries { if ('.pdb','.locked','.pssym' -contains $_.Extension) { $_ } }
-filter intellisenseXml { if(((Split-Path -Leaf $_.Directory) -eq 'bin') `
-    -and ($_.Extension -eq '.xml')) { $_ } }
-filter toolXml { if(($_.DirectoryName.StartsWith("$BuildOutDir\tools\", 'CurrentCultureIgnoreCase')) `
-  -and ($_.Extension -eq '.xml')) { $_ } }
-filter rootBinaries { if (($_.DirectoryName -eq $BuildOutDir) `
-  -and ('.xml','.zip','.dll','.config' -contains $_.Extension)) { $_ } }
-filter symbols { if ($_.Extension -eq '.pdb') { $_ } }
-filter tests { if (($_.Name -imatch '.*Tests\.dll') -and `
-  ($_.Extension -eq '.dll')) { $_ } }
-filter testFrameworks { if (($_.Name -imatch '.*xunit.*') -or `
-  ($_.Name -imatch '.*autofixture.*') -or ($_.Name -imatch '.*fakeiteasy.*') -or `
-  ($_.Name -imatch '.*semanticcomparison.*')) { $_ } }
+Task Package -Depends Compile, Test {
+  'Publishing a public NuGet package'
+  $sourceNuspec = Join-Path $currentDirectory 'NLog.AirBrake.nuspec'
+  $nuspecPath = Join-Path $BuildOutDir 'NLog.AirBrake.nuspec'
+  Copy-Item $sourceNuspec -Destination $nuspecPath
 
-function Get-FileLists
-{
-  $allFilesRecursive = Get-ChildItem $BuildOutDir -Recurse |
-    ? { -not $_.PsIsContainer }
+  $nuspec = [Xml](Get-Content $nuspecPath)
+  $compiled = Join-Path $BuildOutDir 'NLog.AirBrake.dll'
+  $version = (Get-Command $compiled).FileVersionInfo.FileVersion
+  $nuspec.package.metadata.version = $version
+  $nuspec.Save($nuspecPath)
 
-  $allExcludes =
-    (($allFilesRecursive | rootBinaries) +
-      ($allFilesRecursive | binaries) +
-      ($allFilesRecursive | toolXml) +
-      ($allFilesRecursive | intellisenseXml) +
-      ($allFilesRecursive | tests) +
-      ($allFilesRecursive | testFrameworks)) |
-    Select -ExpandProperty FullName
-
-  $fileExcludes = $allExcludes + `
-    ($serviceFiles | Select -ExpandProperty FullName)
-
-  $files = $allFilesRecursive |
-    ? { $fileExcludes -notcontains $_.FullName }
-
-  #Symbols
-  $symbols = $allFilesRecursive |
-    symbols
-
-  return $files, $symbols
-}
-
-Task RemoveTemporaryAssets {
-  #after everything is packaged
-}
-
-Task Package -Depends Compile, RemoveTemporaryAssets {
-  'Retrieving list of artifacts to zip'
-  $files, $symbols = Get-FileLists
-
-  #Zip it up yo!
-
-  Set-7ZipPath $7ZipPath
-  "Creating $ZipPath from files"
-  New-ZipFile -Path $ZipPath -SourceFiles $files `
-    -Root $BuildOutDir -Type zip
-
-  "Creating $ZipSymbolsPath from symbols"
-  New-ZipFile -Path $ZipSymbolsPath -SourceFiles $symbols `
-    -Root $BuildOutDir -Type zip
+  Publish-NugetPackage -Path $BuildOutDir -Force
 }
 
 Task Clean -Depends Init {
   "Removing old zips"
 
-  #old cruft if it's hanging around
   #old cruft if it's hanging around
   if (Test-Path $BuildOutDir)
   {
@@ -116,6 +61,5 @@ Task Init {
 }
 
 Task Test -Depends Compile {
-  # TODO: write some tests!
-  Invoke-Xunit -Path $BuildOutDir #-ExcludeTraits @{"Category" = "Integration"}
+  Invoke-Xunit -Path $BuildOutDir
 }
